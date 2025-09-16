bab2985a463250022e61aecf8dccb8c33a58fb6b
Ailton Junio <ailtonjunio97@gmail.com>
2025-09-16 15:28:56 -0300

    Criado Projeto

---
+++ PI - VI - A/Projeto-Feirinha/src/main/java/com/AJ/Spring/Projeto_Feirinha/ProjetoFeirinhaApplication.java
@@ -0,0 +1,13 @@
+package com.AJ.Spring.Projeto_Feirinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFeirinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFeirinhaApplication.class, args);
+	}
+
+}
---
+++ PI - VI - A/Projeto-Feirinha/src/test/java/com/AJ/Spring/Projeto_Feirinha/ProjetoFeirinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AJ.Spring.Projeto_Feirinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFeirinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
